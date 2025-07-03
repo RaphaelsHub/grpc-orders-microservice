0058ba5100e5a63c7266f3a1b5c533dbba39d98e
Alexandr Pekel <spekel2003@gmail.com>
2025-07-03 14:50:31 +0300

    Add gRPC client and order.proto definitions
    Introduces a gRPC client in Program.cs to interact with an OrderService, supporting order creation, retrieval, listing, and deletion. Adds the order.proto file defining the Order entity, request/response messages, and the OrderService gRPC service contract.

---
+++ ClientSide/Program.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Net.Client;
+using OrdersService.Protos;
+
+Console.WriteLine("Starting gRPC client...");
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5218");
+var client = new OrderService.OrderServiceClient(channel);
+
+var createResponse = await client.CreateOrderAsync(new CreateOrderRequest
+{
+    CustomerName = "Artem",
+    Product = "Laptop",
+    Quantity = 2
+});
+Console.WriteLine($"Order created. ID: {createResponse.Id}");
+
+var orderResponse = await client.GetOrderAsync(new GetOrderRequest
+{
+    Id = createResponse.Id
+});
+Console.WriteLine($"Order fetched: {orderResponse.Order.CustomerName} — {orderResponse.Order.Product} x{orderResponse.Order.Quantity}");
+
+var allOrders = await client.GetAllOrdersAsync(new Empty());
+Console.WriteLine($"\nAll Orders:");
+foreach (var o in allOrders.Orders)
+    Console.WriteLine($"• [{o.Id}] {o.CustomerName} ordered {o.Product} x{o.Quantity}");
+
+var wasDeleted = await client.DeleteOrderAsync(new DeleteOrderRequest
+{
+    Id = createResponse.Id
+});
+Console.WriteLine($"Order Deleted Status {wasDeleted}");
